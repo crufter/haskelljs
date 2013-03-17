d12d482e7417ad80aac537d89cc99e542f21b58f
Dobronszki János <janosdobronszki@citromail.hu>
2013-03-17 18:19:34 +0100

    Still not ready yet.

--- haskell.js
+++ haskell.js
@@ -1,6 +1,6 @@
-var h = function(exports) {
+var h = function(e) {
 
-    exports.map = function (f, a) {
+    e.map = function (f, a) {
         var a1 = []
         for (var i in a) {
             a1.push(f(a[i]))
@@ -8,7 +8,7 @@ var h = function(exports) {
         return a1
     }
     
-    exports.filter = function (f, a) {
+    e.filter = function (f, a) {
         var a1 = []
         for (var i in a) {
             var c = a[i]
@@ -19,42 +19,42 @@ var h = function(exports) {
         return a1
     }
     
-    exports.head = function(a) {
+    e.head = function(a) {
         return a[0]
     }
     
-    exports.last = function(a) {
+    e.last = function(a) {
         return a[a.length - 1]
     }
     
-    exports.tail = function(a) {
+    e.tail = function(a) {
         return a.slice(1)
     }
     
-    exports.init = function(a) {
+    e.init = function(a) {
         return a.slice(0, -1)
     }
     
-    exports.null = function(a) {
+    e.null = function(a) {
         return a.length == 0
     }
     
-    exports.reverse = function(a) {
+    e.reverse = function(a) {
         return a.reverse
     }
     
-    exports.length = function(a) {
+    e.length = function(a) {
         return a.length
     }
     
-    exports.foldl = function(f, s, a) {
+    e.foldl = function(f, s, a) {
         for (var i in a) {
             s = f(s, a[i])
         }
         return s
     }
     
-    exports.foldl1 = function(f, a) {
+    e.foldl1 = function(f, a) {
         var s = a[0]
         for (var i = 1; i < a.length; a++) {
             s = f(s, a[i])
@@ -62,14 +62,14 @@ var h = function(exports) {
         return s
     }
     
-    exports.foldr = function(f, s, a) {
+    e.foldr = function(f, s, a) {
         for (var i = a.length - 1; i>=0; i--) {
             s = f(s, a[i])
         }
         return s
     }
     
-    exports.foldr1 = function(f, a) {
+    e.foldr1 = function(f, a) {
         var s = a[a.length - 1]
         for (var i = a.length - 2; i >= 0; a--) {
             s = f(s, a[i])
@@ -77,7 +77,7 @@ var h = function(exports) {
         return s
     }
     
-    exports.and = function(a) {
+    e.and = function(a) {
         for (var i in a) {
             if (a[i] == false) {
                 return false
@@ -86,7 +86,7 @@ var h = function(exports) {
         return true
     }
     
-    exports.or = function(a) {
+    e.or = function(a) {
         for (var i in a) {
             if (a[i] == true) {
                 return true
@@ -95,7 +95,7 @@ var h = function(exports) {
         return false
     }
     
-    exports.any = function(f, a) {
+    e.any = function(f, a) {
         for (var i in a) {
             if (f(a[i]) == true) {
                 return true
@@ -104,7 +104,7 @@ var h = function(exports) {
         return false
     }
     
-    exports.all = function(f, a) {
+    e.all = function(f, a) {
         for (var i in a) {
             if (f(a[i]) == false) {
                 return false
@@ -113,7 +113,7 @@ var h = function(exports) {
         return true
     }
     
-    exports.sum = function(a) {
+    e.sum = function(a) {
         var s = 0
         for (var i in a) {
             s += f(a[i])
@@ -121,7 +121,7 @@ var h = function(exports) {
         return true
     }
     
-    exports.product = function(a) {
+    e.product = function(a) {
         var s = 0
         for (var i in a) {
             s *= f(a[i])
@@ -129,31 +129,31 @@ var h = function(exports) {
         return true
     }
     
-    exports.concat = function(a) {
+    e.concat = function(a) {
         var r = []
         for (var i in a) {
             r.concat(a[i])
         }
     }
     
-    exports.concatMap = function(f, a) {
+    e.concatMap = function(f, a) {
         var r = []
         for (var i in a) {
             r.concat(f(a[i]))
         }
     }
     
-    exports.maximum = function(a) {
+    e.maximum = function(a) {
         return Math.max.apply(Math, a)
     }
     
-    exports.minimum = function(a) {
+    e.minimum = function(a) {
         return Math.min.apply(Math, a)
     }
     
     // scanl, scanl1, scanr, scanr1
     
-    exports.replicate = function(i, e) {
+    e.replicate = function(i, e) {
         var a = []
         for (var x = 0; x<i; x++) {
             a.push(e)
@@ -161,7 +161,167 @@ var h = function(exports) {
         return a
     }
     
+    e.take = function(n, a) {
+        var alen = a.length
+        if (n > alen) {
+            return a
+        }
+        var a1 = []
+        for (var i = 0; i < n && i < alen; i++) {
+            a1.push(a[i])
+        }
+        return a1
+    }
     
+    e.drop = function(n, a) {
+        var alen = a.length
+        if (n > alen) {
+            return []
+        }
+        var a1 = []
+        for (var i = n; i < alen; i++) {
+            a1.push(a[i])
+        }
+        return a1
+    }
     
-    return exports
+    e.splitAt = function(n, a) {
+        return [e.take(n, a), e.drop(n, a)]
+    }
+    
+    e.takeWhile = function(pred, a) {
+        var a1 = []
+        for (var i in a) {
+            if (pred[a[i]] == true) {
+                a1.push[a[i]]
+            }
+        }
+        return a1
+    }
+    
+    e.dropWhile = function(pred, a) {
+        var a1 = []
+        for (var i in a) {
+            if (pred[a[i]] == false) {
+                return a.slice(0)
+            }
+        }
+        return []
+    }
+    
+    e.span = function(pred, a) {
+        var sat = e.takeWhile(pred, a)
+        return [sat, a.slice(sat.length)]
+    }
+    
+    e.break = function(pred, a) {
+        var pred1 = function(c) {
+            return !pred(c)
+        }
+        return e.span(pred1, a)
+    }
+    
+    e.elem = function(e, a) {
+        for (var i in a) {
+            if (e == a[i]) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    e.notElem = function(e, a) {
+        return !e.elem(e, a)
+    }
+    
+    // lookup
+    
+    e.zip = function(a1, a2) {
+        var a3 = []
+        var a2len = a2.length
+        for (var i in a1) {
+            if (i <= a2len) {
+                return a3
+            }
+            a3.push([a1[i], a2[i]])
+        }
+        return a3
+    }
+    
+    e.zip3 = function(a1, a2, a3) {
+        var a4 = []
+        var a2len = a2.length, a3len = a3.length
+        for (var i in a1) {
+            if (i <= a2len || i <= a3len) {
+                return a4
+            }
+            a4.push([a1[i], a2[i], a3[i]])
+        }
+        return a4
+    }
+    
+    e.zipWith = function(f, a1, a2) {
+        return false
+    }
+    
+    e.zipWith3 = function(f, a1, a2, a3) {
+        return false
+    }
+    
+    e.unzip = function(f, a1, a2) {
+        return false
+    }
+    
+    e.unzip3 = function(f, a1, a2, a3) {
+        return false
+    }
+    
+    e.lines = function(s) {
+        return s.split("\n")
+    }
+    
+    e.words = function(s) {
+        return s.split(" ")
+    }
+    
+    e.unwords = function(a) { 
+        return a.join(" ")
+    }
+    
+    e.unlines = function(a) {
+        return a.join("\n")
+    }
+    
+    //
+    
+    e.tests = function() {
+        var assert = function (val, tname) {
+            if(!val) {
+                console.log(tname + " test failed.")
+                return
+            }
+            return
+        }
+        var arraysEqual = function (a1, a2) {
+            return JSON.stringify(a1)==JSON.stringify(a2)
+        }
+        var sqrt = Math.sqrt
+        assert(arraysEqual(e.map(sqrt, [1, 2, 3]), [sqrt(1), sqrt(2), sqrt(3)]), "map")
+        var isEven = function(a) {
+            return a%2 == 0
+        }
+        assert(arraysEqual([2,4], e.filter(isEven, [1,2,3,4,5])), "filter")
+        assert(e.head([2,3,4]) == 2, "head")
+        assert(e.head([3]) == 3, "head")
+        assert(e.last([2,3,4]) == 4, "last")
+        assert(e.last([3]) == 3, "last")
+        assert(arraysEqual(e.init([2,3,4]), [2,3]), "init")
+        assert(arraysEqual(e.tail([1,2,3]), [2,3]), "tail")
+        assert(e.null([1]) == false, "null")
+        assert(e.null([]) == true, "null")
+        assert(arraysEqual(e.reverse([3,4,5,6]), [6,5,4,3]), "reverse")
+        assert(e.length([0,0,0]) == 3, "length")
+    }
+    
+    return e
 }({})
